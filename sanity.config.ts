0d4225ef8c173a257e579fdbd0e677d7e68a1793
Gesellschaften-direkt <info@gesellschaften-direkt.de>
2025-04-23 22:26:34 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
-import { schemaTypes } from './schemas/schema'
+import schemaTypes from './schemas/schema'
 
 export default defineConfig({
   name: 'default',
   title: 'Gesellschaften CMS',
-  projectId: 'your_project_id', // <== DEINE ID hier eintragen!
+  projectId: '3hi0th2w',
   dataset: 'production',
   plugins: [deskTool()],
   schema: {
-    types: schemaTypes,
-  },
+    types: schemaTypes
+  }
 })
